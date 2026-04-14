16776c5ca93de862984b5b6143901dd33de58c9f
Akshaya22123 <akshayakotha22@gmail.com>
2026-04-14 14:26:32 +0530

    coded UC2: Display Hello with command-Line Argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,10 @@
 public class HelloApp{
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        // Accessing the first argument from the command line
+
+        String name = "akshaya";
+        name=args[0];
+
+        System.out.println("Hello, " + name + "!");
     }
 }
